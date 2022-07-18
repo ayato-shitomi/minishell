ba2ab49491ced7e24a172a011be959485b97a79d
zou <user@USERnoMacBook-ea.local>
2022-07-18 10:15:19 +0900

    大まかな流れ4

--- minishell_utils.c
+++ minishell_utils.c
@@ -6,7 +6,7 @@
 /*   By: mhida <mhida@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 09:22:44 by mhida             #+#    #+#             */
-/*   Updated: 2022/07/18 09:22:46 by mhida            ###   ########.fr       */
+/*   Updated: 2022/07/18 10:14:59 by mhida            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	is_quote(char c, t_info *info)
 	if (c == '\"' || c == '\'')
 	{
 		info->is_in_quote++;
-		return (0);
+		return (1);
 	}
-	return (1);
+	return (0);
 }
 
 int	is_space(char c, t_info *info)
@@ -27,7 +27,7 @@ int	is_space(char c, t_info *info)
 	if (c == ' ')
 	{
 		info->is_in_space++;
-		return (0);
+		return (1);
 	}
-	return (1);
+	return (0);
 }
