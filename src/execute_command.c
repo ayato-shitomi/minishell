89cf458197302465f4f180f6fa888a377ac6687d
Ayato Shitomi <ashitomi@c1r13s2.42tokyo.jp>
2022-08-18 17:19:43 +0900

    norm fix on  src/execute_command.c on Thu Aug 18 17:19:43 JST 2022

--- src/execute_command.c
+++ src/execute_command.c
@@ -1,5 +1,12 @@
 #include "../includes/minishell.h"
 
+void	put_exitstatus(int n)
+{
+	if (n != 0)
+		printf("\x1b[31m[%d]\x1b[0m ", n);
+	else
+		printf("\x1b[32m[%d]\x1b[0m ", n);
+}
 
 int	execute_command(t_info *info)
 {
@@ -8,7 +15,6 @@ int	execute_command(t_info *info)
 	pid_t			pid;
 	pid_t			w_pid;
 	extern char		**environ;
-	int				n;
 
 	sentence_lst_tmp = info->sentence_lst;
 	if (fork_and_error_check(&pid) == ERROR)
@@ -25,11 +31,7 @@ int	execute_command(t_info *info)
 		set_sig_in_parent_process(info);
 		w_pid = waitpid(pid, &status, WUNTRACED);
 	}
-	n = WEXITSTATUS(status);
-	if (n != 0)
-		printf("\x1b[31m[%d]\x1b[0m ", n);
-	else
-		printf("\x1b[32m[%d]\x1b[0m ", n);
+	put_exitstatus(WEXITSTATUS(status));
 	info->sentence_lst = sentence_lst_tmp;
 	return (SUCCESS);
 }
