457119741a5c2fe3d8d459c1b06d1c6b0a446c9a
Ayato Shitomi <ashitomi@c1r13s2.42tokyo.jp>
2022-09-05 22:59:10 +0900

    making tester on Mon Sep  5 22:59:10 JST 2022

---
+++ tester/test.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#=============================================================#
+
+TEST_DIR=`pwd`
+clear
+cd ..
+make
+DIR=`pwd`
+
+RS=$TEST_DIR/result_log.md
+F1=$TEST_DIR/minishell_stdout
+F2=$TEST_DIR/bash_stdout
+
+echo "`date`" > $RS
+
+#============================================================#
+
+function write_back_qote()
+{
+	echo '```' >> $RS
+}
+
+function runtest()
+{
+	clear
+	
+	echo "$@ > $F1" | ./minishell 2>&-
+	echo "$@ > $F2" | bash 2>&-
+
+	DIFF=`diff $F1 $F2`
+	if [ $? -ne 0 ]; then
+		echo "## $@ : >> KO << " >> $RS
+		echo "" >> $RS
+		echo "minishell" >> $RS
+		write_back_qote
+		cat $F1 >> $RS
+		write_back_qote
+		echo "bash" >> $RS
+		write_back_qote
+		cat $F2 >> $RS
+		write_back_qote
+	else
+		echo "## $@ : OK" >> $RS
+	fi
+	sleep 0.1
+	clear
+}
+
+#=============================================================#
+
+runtest 'echo hello'
+runtest 'echo "hello world"'
+runtest 'echo $?'
+runtest 'echo "$?"'
+runtest 'echo '$?''
+runtest 'echo "hello"world hello'
+
+runtest 'pwd'
+
+runtest 'cd ..'
+runtest 'cd ~'
+
+runtest 'ls'
+runtest 'ls -la'
+
+#=============================================================#
+
+glow $RS
