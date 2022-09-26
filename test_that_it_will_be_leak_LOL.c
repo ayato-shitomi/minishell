408d529c72703bd4853569f70b1404dc820b90da
Ayato Shitomi <ashitomi@c1r13s2.42tokyo.jp>
2022-09-26 20:10:49 +0900

    all done, im mad on Mon Sep 26 20:10:49 JST 2022

--- test_that_it_will_be_leak_LOL.c
+++ test_that_it_will_be_leak_LOL.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+
+/*
+ * NOTE
+ *
+ * - run `gcc FILE_NAME` to build.
+ * - run `ps | grep a.out` to get PID.
+ * - run `leak PID`. to check this program making leaks.
+ *
+ * >>> WE WERE NOT MAKING MISTAKES <<<
+ *
+*/
+
 int	main(void)
 {
 	char *p;
